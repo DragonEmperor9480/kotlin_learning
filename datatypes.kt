a9db6b58b025b6043c9da9315318d1f531e4d142
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-11 12:51:45 +0530

    Datatypes in Kotlin

---
+++ datatypes.kt
@@ -0,0 +1,33 @@
+
+
+// There are different data types in Kotlin:
+
+// Integer Data type
+// Floating-point Data Type
+// Double Data Type
+// Boolean Data Type
+// Character Data Type
+// String Data Type
+
+
+
+fun main()
+{
+    // Suppose we need to define the Datatype we follow the below method
+    val a: Int = 10;
+    val b: Double = 10.0
+    val c: Char = 'A'
+    val d: String = "Ayaka"
+    val e: Boolean = true
+    val f: Float = 10F
+  // add suffix F for float 
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    // We cannot Keep the double value as integer. It will throw an error
+    // Boolean can be either true or false
+}
