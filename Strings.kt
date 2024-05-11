4572da8ff8b0d8137c25621ab7b75d5a052f8f67
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-11 18:54:51 +0530

    Strings in Kotlin

---
+++ Strings.kt
@@ -0,0 +1,19 @@
+fun main()
+{
+    val firstname = "Amrut"
+    val lastname = "Naregal"
+    val temp = "temp"
+println("My FirstName is $firstname and lastname is $lastname")
+
+//to find length of string
+println(firstname.length)
+
+//Capitalize
+println(temp.capitalize())
+
+//uppercase
+println(temp.toUpperCase())
+
+//Lowercase
+println(lastname.toLowerCase())
+}
