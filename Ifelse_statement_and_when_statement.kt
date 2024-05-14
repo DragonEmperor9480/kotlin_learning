c2fdba18f9a8cf1f8be43c94ff23246ad156b581
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-14 20:28:08 +0530

    if else and when conditions

---
+++ Ifelse_statement_and_when_statement.kt
@@ -0,0 +1,51 @@
+//if else statement
+fun main()
+{
+    var a=2
+    if(a==1){
+    println(a)
+    }
+    else
+    {
+        println("error")
+    }
+
+//if else ladder
+ a =5
+if(a==1)
+{
+    println("1")
+}
+else if(a==2)
+{
+    println("2")
+}
+else if(a==3)
+{
+    println("3")
+}
+else if(a==4)
+{
+    println("4")
+}
+else if(a==5)
+{
+    println("5")
+}
+else{
+    println("Error")
+}
+//When condition
+//so like in the above if else ladder its just big to write so we will use when condition to write
+when(a)
+{
+    1->println("1")
+    2->println("2")
+    3->println("3")
+    4->println("4")
+    5->println("5")
+    else-> println("Error")
+}
+
+}
+
