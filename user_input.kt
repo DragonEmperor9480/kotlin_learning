254921fbd941c707c85934918c84f4a4e7359312
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-05-27 09:10:51 +0530

    How to take user input[1/3]

---
+++ user_input.kt
@@ -0,0 +1,10 @@
+import  java.util.Scanner;
+
+fun main()
+{
+println("Enter value of A")    
+val a: Int = readLine()!!.toInt()
+println("Enter value of B")    
+val b: Int = readLine()!!.toInt()
+println(a+b)
+}
